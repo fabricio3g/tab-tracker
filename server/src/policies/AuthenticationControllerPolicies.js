59f972846a74d133bce87dcfbfab89c4929c559c
fabricio3g <fabricio.3g@gmail.com>
2021-10-31 20:23:50 -0300

    validation messeges added

--- server/src/policies/AuthenticationControllerPolicies.js
+++ server/src/policies/AuthenticationControllerPolicies.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 
 module.exports = {
-    register(req, res, next){
+    async register(req, res, next){
 
         const schema = Joi.object({
             email: Joi.string().email(),
@@ -11,9 +11,36 @@ module.exports = {
 
         const {error , value } = schema.validate({email: req.body.email, password: req.body.password})
 
+        if(error !== undefined){
+                switch(error.details[0].context.key){
+                
+                    case 'email':
+                        res.status(400)
+                            .send({
+                                error: 'You must provide a valid email adress'
+                            })
+                        break
+                    case 'password':
+                        res.status(400)
+                            .send({
+                                error: 'You must provide a valid password'
+                            })
+                        break
+                    default:
+                        res.status(400)
+                        .send({
+                            error: 'Invalid registration information'
+                        })
+                        break
+
+                }
+        }
+        else{
+            console.log(value)
+            await next()
+        }
 
         
-
-        next()
+        
     }
 }
