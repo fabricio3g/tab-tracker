732490da3874d01e55f892e55e806c32a4ef2dad
fabricio3g <fabricio.3g@gmail.com>
2021-11-01 21:55:42 -0300

    Update config.js

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 5051,
+    port: process.env.PORT || 5052,
     db:{
         database: process.env.DB_NAME || 'tabtracker',
         user: process.env.DB_USER || 'tabtracker',
