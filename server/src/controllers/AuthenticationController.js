c415f1822e720d7afde2a8ace5cad10ffe225969
fabricio3g <fabricio.3g@gmail.com>
2021-11-01 21:56:12 -0300

    login controller added

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -5,12 +5,59 @@ module.exports  = {
     async register(req ,res){
        
         try{
-            const user = await User.create({email: req.body.email, passowrd: req.body.password})
+            const user = await User.create({email: req.body.email, password: req.body.password})
+            console.log(user.toJSON())
             res.send(user.toJSON())
-        }
-        catch(err){
+        } catch(err){
+            console.log(err)
             res.status(400).send({
                 error: 'This email is already in use'
+            })
+        }
+          
+    },
+    async login (req, res) {
+
+        const {email , password} = req.body
+
+
+        try{
+            const user = await User.findOne({
+                where:{
+                    email:email
+                }
+            })
+
+           
+           
+            if(!user){
+                return res.status(403).send({
+                    error: 'The login information was incorrent'
+                })
+            }
+            
+            const isPasswordIsValid = () =>{
+                return password === user.password
+            }
+
+            
+
+            if(!isPasswordIsValid()){
+                
+                return res.status(403).send({
+                    error: 'The login information was incorrent'
+                })
+            }
+           
+           const userJson = user.toJSON()
+           
+            return res.send({
+               user: userJson
+            })
+        } catch(err){
+            console.log(err)
+            res.status(500).send({
+                error: 'An error has occured trying to login'
             })
         }
           
