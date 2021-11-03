4f517ffa0e9c52f716cf9629bd7d71cc8f5683be
fabricio3g <fabricio.3g@gmail.com>
2021-11-03 03:13:31 -0300

    vuex store sync configured

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import route from './router'
-createApp(App).use(route).mount('#app')
+import store from './store'
+import { sync } from 'vuex-router-sync'
+
+
+sync(store, route)
+
+
+createApp(App).use(store).use(route).mount('#app')
