dbcffe3e906c189a4a9bf5c09029e92c30e29bc0
fabricio3g <fabricio.3g@gmail.com>
2021-11-06 02:22:20 -0300

    Panel component added

---
+++ client/src/components/Panel.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card">
+        <div class="card-header" style="text-aling:center">{{title}}</div>
+        <div class="card-body">
+            <slot>
+                No content at all
+            </slot>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  props: ['title']
+}
+</script>
+
+<style scoped>
+    .card{
+        margin:2rem auto;
+        width: 500px;
+    }
+
+</style>
