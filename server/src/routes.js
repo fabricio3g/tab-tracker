d9c36f3238c61a8a07afc2fb86452941f39b053b
fabricio3g <fabricio.3g@gmail.com>
2021-11-06 05:12:41 -0300

    new routes added

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,11 @@
 const AutheticationController = require('./controllers/AuthenticationController')
 const AuthenticationControllerPolicies = require('./policies/AuthenticationControllerPolicies')
+const SongsController = require('./controllers/SongsControllers')
 
 module.exports = ( app ) => {
     app.post('/register', AuthenticationControllerPolicies.register ,AutheticationController.register),
     app.post('/login', AutheticationController.login)
+    app.get('/songs', SongsController.index)
+    app.post('/song-create', SongsController.post)
 }
+
