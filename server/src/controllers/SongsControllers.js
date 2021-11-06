59251c11d7ffae3f7abc9bd9425f8778a967e6c6
fabricio3g <fabricio.3g@gmail.com>
2021-11-06 05:12:09 -0300

    Create SongsControllers.js

---
+++ server/src/controllers/SongsControllers.js
@@ -0,0 +1,45 @@
+const { Songs }  = require('../models')
+
+
+module.exports  = {
+    
+    async index ( req ,res ){
+
+        try{
+          const song = await Songs.findAll({
+              limit: 10
+          })
+          res.send(song)
+        } catch(err){
+            console.log(err)
+            res.status(500).send({
+                error: 'An error has occured trying to fetch the songs'
+            })
+        }
+          
+    },
+    async post ( req ,res ){
+        const {title, artist, genere, albumImage, youtubeId, lyric, tab, album} = req.body
+
+        try{
+          const song = await Songs.create({
+            title,
+            artist,
+            genere,
+            album,
+            albumImage,
+            youtubeId,
+            lyric,
+            tab
+          })
+          res.send(song)
+        } catch(err){
+            console.log(err)
+            res.status(500).send({
+                error: 'An error has occured trying to create the song'
+            })
+        }
+          
+    }
+
+}
