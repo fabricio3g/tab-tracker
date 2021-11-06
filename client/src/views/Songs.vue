b4eda24970a3abe0c7c2270408933f253aae07bc
fabricio3g <fabricio.3g@gmail.com>
2021-11-06 05:11:55 -0300

    Songs view

---
+++ client/src/views/Songs.vue
@@ -0,0 +1,62 @@
+
+<template>
+    <Panel title='Songs'>
+        <div class="card-container">
+              <div class="card card-song" v-for="song in songs" :key="song.id">
+                <img class="card-img-top" v-bind:src="song.albumImage" alt="Card image cap">
+                  <div class="card-body">
+                    <h4 class="card-title">{{song.title}}</h4>
+                    <p class="card-text">Artist: {{song.artist}}</p>
+                  </div>
+                  <div class="card-footer">
+                  <small class="text-muted">Genere: {{song.genere}}</small><br>
+                  <button class="btn btn-primary" @click.prevent="navegateTo({name: 'song', params: { songId: song.id} })">View</button>
+            </div>
+          </div>
+          </div>
+    </Panel>
+</template>
+
+<script>
+import Panel from '../components/Panel.vue'
+import SongServices from '../services/SongServices'
+export default {
+  data () {
+    return {
+      songs: null
+    }
+  },
+  components: {
+    Panel
+  },
+  async mounted () {
+    // request songs to the backend
+    this.songs = (await SongServices.index()).data
+  },
+  methods: {
+    navegateTo (obj) {
+      // const name = obj.name
+      // const songId = obj.params.songId
+      this.$router.push(obj)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .card{
+    width: 70%;
+  }
+  .card-img-top{
+    width: 200px;
+  }
+  .card-song{
+    width: 200px;
+    margin:1rem auto;
+  }
+  .card-container{
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+</style>
