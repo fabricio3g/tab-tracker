789eb991800b98554b83af5f4769e9591dab414a
fabricio3g <fabricio.3g@gmail.com>
2021-11-07 23:48:47 -0300

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,6 @@ import App from './App.vue'
 import route from './router'
 import store from './store'
 import { sync } from 'vuex-router-sync'
-
-
 sync(store, route)
-
 
 createApp(App).use(store).use(route).mount('#app')
