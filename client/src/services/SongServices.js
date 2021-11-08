abe7f2ac873ae49109a2f4928ad1f4c316430fe8
fabricio3g <fabricio.3g@gmail.com>
2021-11-08 01:06:01 -0300

    update song  axios

--- client/src/services/SongServices.js
+++ client/src/services/SongServices.js
@@ -7,6 +7,10 @@ export default {
   show (songId) {
     return Api.get(`songs/${songId}`)
   },
+  put (song) {
+    console.log()
+    return Api.put(`songs/${song.songId}`, song)
+  },
   post (song) {
     return Api.post('song-create', song)
   }
