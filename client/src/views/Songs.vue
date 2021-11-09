004236a340152058140677e4dd8f89668f35b5c7
fabricio3g <fabricio.3g@gmail.com>
2021-11-09 02:34:17 -0300

    Update Songs.vue

--- client/src/views/Songs.vue
+++ client/src/views/Songs.vue
@@ -1,7 +1,11 @@
 
 <template>
+    <div>
     <Panel title='Songs'>
-        <router-link  v-if="this.$store.state.isUserLogIn"   class="router__link btn btn-primary" to="/song-create" >Create Song</router-link >
+       <router-link  v-if="this.$store.state.isUserLogIn"   class="router__link btn btn-primary" to="/song-create" >Create Song</router-link >
+      <Panel title='Search' class="search">
+        <input v-model="search" type="text" class="search__song" placeholder="Search by title...">
+      </Panel>
         <div class="card-container">
               <div class="card card-song" v-for="song in songs" :key="song.id">
                 <img class="card-img-top" v-bind:src="song.albumImage" alt="Card image cap">
@@ -16,6 +20,7 @@
           </div>
           </div>
     </Panel>
+    </div>
 </template>
 
 <script>
@@ -24,7 +29,8 @@ import SongServices from '../services/SongServices'
 export default {
   data () {
     return {
-      songs: null
+      songs: null,
+      search: ''
     }
   },
   components: {
@@ -32,11 +38,31 @@ export default {
   },
   async mounted () {
     // request songs to the backend
-    this.songs = (await SongServices.index()).data
+    this.songs = (await SongServices.index(this.search)).data
   },
   methods: {
     navegateTo (obj) {
       this.$router.push(obj)
+    }
+  },
+  watch: {
+    '$route.query.search': {
+      immediate: true,
+      async handler (value) {
+        this.search = value
+        this.songs = (await SongServices.index(value)).data
+      }
+    },
+    search (value) {
+      const route = {
+        name: 'songs'
+      }
+      if (this.search !== '') {
+        route.query = {
+          search: this.search
+        }
+      }
+      this.$router.push(route)
     }
   }
 }
@@ -57,5 +83,13 @@ export default {
     display: flex;
     justify-content: center;
     flex-wrap: wrap;
+  }
+  .search__song{
+    width: 100%;
+    font-family: 'Courier New', Courier, monospace;
+    height: 3rem;
+    padding: 1rem;
+    border-radius: .5rem;
+    border: 1px solid rgb(202, 202, 202);
   }
 </style>
