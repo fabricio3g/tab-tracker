9d3620e91b7beb154b24d1e9be43002c1bdb786e
fabricio3g <fabricio.3g@gmail.com>
2021-11-09 02:36:19 -0300

    Update Register.vue

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -32,6 +32,7 @@ export default {
         })
         await this.$store.dispatch('setToken', response.data.token)
         await this.$store.dispatch('setUser', response.data.token)
+        this.$router.push({ path: '/login' })
       } catch (error) {
         this.error = error.response.data.error
         console.log(error)
