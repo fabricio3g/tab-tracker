59f44d47be9aa10d9e893699bc57564cb0148d5b
fabricio3g <fabricio.3g@gmail.com>
2021-11-09 02:35:16 -0300

    bookmark route

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,13 +1,15 @@
 const AutheticationController = require('./controllers/AuthenticationController')
 const AuthenticationControllerPolicies = require('./policies/AuthenticationControllerPolicies')
 const SongsController = require('./controllers/SongsControllers')
+const BookmarkControllers = require('./controllers/BookmarkControllers')
 
 module.exports = ( app ) => {
-    app.post('/register', AuthenticationControllerPolicies.register ,AutheticationController.register),
+    app.post('/register', AuthenticationControllerPolicies.register, AutheticationController.register),
     app.post('/login', AutheticationController.login)
     app.get('/songs', SongsController.index)
     app.get('/songs/:songId', SongsController.show)
     app.post('/song-create', SongsController.post)
     app.put('/songs/:songId', SongsController.put)
+    app.get('/bookmarks', BookmarkControllers.index)
 }
 
