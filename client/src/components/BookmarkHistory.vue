136ae01e72bd9bcd2a3f7b8e6c7f275e80942d0f
fabricio3g <fabricio.3g@gmail.com>
2021-11-12 15:04:45 -0300

    Create BookmarkHistory.vue

---
+++ client/src/components/BookmarkHistory.vue
@@ -0,0 +1,49 @@
+<template>
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th scope="col"><b>Recent Viewed songs</b></th>
+      </tr>
+    </thead>
+    <thead>
+      <tr>
+        <th scope="col">Title</th>
+        <th scope="col">Artist</th>
+        <th scope="col">View Song</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Feel Good</td>
+        <td>Gorillaz</td>
+        <td> <button class="btn">Go</button></td>
+      </tr>
+    </tbody>
+    <nav class="navegation" aria-label="navigation">
+      <ul class="pagination">
+        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+      </ul>
+    </nav>
+  </table>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .btn{
+        color:black;
+        border: .5px solid rgb(201, 201, 201);
+    }
+    .pagination > li{
+        border: 1px solid rgb(197, 197, 197);
+        padding: 1px;
+        margin: 2px;
+    }
+</style>
