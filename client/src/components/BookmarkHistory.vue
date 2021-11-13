16a48ba2033e1d69a61720ab91b767e861f1f75c
fabricio3g <fabricio.3g@gmail.com>
2021-11-13 02:02:24 -0300

    Update BookmarkHistory.vue

--- client/src/components/BookmarkHistory.vue
+++ client/src/components/BookmarkHistory.vue
@@ -2,7 +2,7 @@
   <table class="table table-hover">
     <thead>
       <tr>
-        <th scope="col"><b>Recent Viewed songs</b></th>
+        <th scope="col"><b>Bookmarks</b></th>
       </tr>
     </thead>
     <thead>
@@ -12,11 +12,11 @@
         <th scope="col">View Song</th>
       </tr>
     </thead>
-    <tbody>
+    <tbody  v-for="song in songs" :key="song.id" >
       <tr>
-        <td>Feel Good</td>
-        <td>Gorillaz</td>
-        <td> <button class="btn">Go</button></td>
+        <td>{{song.Song.title}}</td>
+        <td>{{song.Song.artist}}</td>
+        <td> <button class="btn" @click.prevent="navegateTo({name: 'song', params: { songId: song.Song.id} })">Go</button></td>
       </tr>
     </tbody>
     <nav class="navegation" aria-label="navigation">
@@ -31,7 +31,25 @@
   </table>
 </template>
 <script>
+import BookmarkServices from '../services/BookmarkServices'
 export default {
+  data () {
+    return {
+      songs: []
+    }
+  },
+  async mounted () {
+    console.log(this.$store.state.user.id)
+    const data = (await BookmarkServices.index({
+      userId: this.$store.state.user.id
+    })).data
+    this.songs = data
+  },
+  methods: {
+    navegateTo (obj) {
+      this.$router.push(obj)
+    }
+  }
 
 }
 </script>
@@ -41,9 +59,19 @@ export default {
         color:black;
         border: .5px solid rgb(201, 201, 201);
     }
+    .table{
+      margin: 10px auto;
+      width: 800px;
+    }
     .pagination > li{
         border: 1px solid rgb(197, 197, 197);
         padding: 1px;
         margin: 2px;
     }
+
+    @media (max-width: 767px){
+    .table{
+      width: 300px !important;
+    }
+  }
 </style>
