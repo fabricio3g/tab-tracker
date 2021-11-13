af9bd118b35efb95be3ccb58648268db1bbdc329
fabricio3g <fabricio.3g@gmail.com>
2021-11-13 11:23:26 -0300

    isAuthenticated jwt

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,17 +2,17 @@ const AutheticationController = require('./controllers/AuthenticationController'
 const AuthenticationControllerPolicies = require('./policies/AuthenticationControllerPolicies')
 const SongsController = require('./controllers/SongsControllers')
 const BookmarkControllers = require('./controllers/BookmarkControllers')
-
+const isAuthenticated = require('./policies/isAuthenticated')
 module.exports = ( app ) => {
     app.post('/register', AuthenticationControllerPolicies.register, AutheticationController.register),
     app.post('/login', AutheticationController.login)
     app.get('/songs', SongsController.index)
     app.get('/songs/:songId', SongsController.show)
-    app.post('/song-create', SongsController.post)
-    app.put('/songs/:songId', SongsController.put)
+    app.post('/song-create',isAuthenticated,SongsController.post)
+    app.put('/songs/:songId', isAuthenticated , SongsController.put)
 
-    app.get('/bookmarks', BookmarkControllers.index)
-    app.post('/bookmarks', BookmarkControllers.post)
+    app.get('/bookmarks', isAuthenticated, BookmarkControllers.index)
+    app.post('/bookmarks', isAuthenticated ,BookmarkControllers.post)
     app.delete('/bookmarks', BookmarkControllers.delete)
 }
 
