bd7775f3433bbb024ecc4c5dd0ece0419580cf15
fabricio3g <fabricio.3g@gmail.com>
2021-11-13 11:25:12 -0300

    header authorization added

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,5 +1,11 @@
 import axios from 'axios'
-
-const Api = axios.create({ baseURL: 'http://localhost:5052/' })
+import store from '../store/index'
+console.log(store.state.token)
+const Api = axios.create({
+  baseURL: 'http://localhost:5052/',
+  headers: {
+    Authorization: 'Bearer ' + store.state.token
+  }
+})
 
 export default Api
